0d8f80609373b4e61685024540e9aab690239461
sinstar2000 <strongweb102@gmail.com>
2018-09-03 01:13:28 -0700

    add create action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,4 +3,12 @@ class TasksController < ApplicationController
 		puts "Honey, I'm home!"
 		@task = Task.new
 	end
+	def create
+		@task = Task.create(task_params)
+		redirect_to root_path
+	end
+	private
+	def task_params
+		params.require(:task).permit(:title, :note, :completed)
+	end
 end
