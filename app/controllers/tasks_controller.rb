9a920ed58a925890db999a80132dca581916d018
sinstar2000 <strongweb102@gmail.com>
2018-09-03 05:26:50 -0700

    add delete action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,7 +5,12 @@ class TasksController < ApplicationController
 	end
 	def create
 		@task = Task.create(task_params)
-		redirect_to root_path
+    @tasks = Task.all
+	end
+	def destroy
+		@task = Task.find(params[:id])
+		@task.destroy
+		@tasks = Task.all
 	end
 	private
 	def task_params
